fa84210c7a03045bfc130bd18ee23a3f737c9043
phpjscoding <phpjscoder@gmail.com>
2022-04-10 15:02:30 +0000

    about,layout,css module

---
+++ src/components/Layout.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import {Link} from 'gatsby'
+import {container,mainNav} from './layout.module.css'
+
+
+
+const Layout = ({pageTitle,children})=>{
+	return(
+		  <div className={container}>
+		  	    <title>{pageTitle}</title>
+		  	    <nav className={mainNav}>
+		  	    	<ul>
+		  	    		<li><Link to='/'>Home</Link></li>
+		  	    		<li><Link to="/about">About</Link></li>
+		  	    	</ul>
+		  	    </nav>
+
+		  	    
+		  	    <main>
+		  	    	<h1> {pageTitle} </h1>
+		  	    	{children}
+		  	    </main>
+		  </div>
+		)
+}
+
+
+export default Layout
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+
+const About = ()=>{
+  return(
+       <Layout pageTitle="about-us">
+         <h2> this is about us page</h2>
+       </Layout>
+    )
+}
+
+export default About
